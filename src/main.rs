a063c757ab63f34d70255caf15b4831bd0883659
Ivan Oštrić <ivan@halcyon.hr>
2019-05-06 22:45:14 +0200

    More Rust-y main function

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,14 @@ use std::io::BufReader;
 
 mod parser;
 
-fn main() {
+fn main() -> Result<(), std::io::Error>{
     let _args: Vec<String> = env::args().collect();
 
     const DATA_FILENAME: &str = "data/emoji-test.txt";
-    let data_file = fs::File::open(DATA_FILENAME).unwrap();
+    let data_file = match fs::File::open(DATA_FILENAME) {
+        Ok(file) => file,
+        Err(e) => return Err(e)
+    };
 
     let reader = BufReader::new(data_file);
     let emojis = parser::read_emoji_file(reader);
@@ -18,6 +21,7 @@ fn main() {
     for emoji in emojis {
         println!("{} [{}] [{}] [{}]", emoji.description, emoji.group, emoji.subgroup, emoji.codepoint);
     }
-}
 
+    Ok(())
+}
 
